9b66fad50e59840da49e1d5d1fa791a17b8c6736
Phong Nguyen <nguyenhongphong1010.02@gmail.com>
2024-12-15 23:36:38 +0700

    Update HomeScreen
    Add handleNavigateToProductDetail callback and update CSS styles

--- src/screens/bottom_navs/HomeScreen.jsx
+++ src/screens/bottom_navs/HomeScreen.jsx
@@ -5,21 +5,25 @@ import {
   Text,
   FlatList,
   Dimensions,
-  Touchable,
   TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
 import React from 'react';
 import ScreenEnum from '../../constants/screenEnum';
 
-const {width, height} = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 
 const HomeScreen = props => {
   const {navigation} = props;
 
+  const handleNavigateToProductDetail = () => {
+    navigation.navigate(ScreenEnum.ProductDetail);
+  };
+
   return (
-    <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
+    <SafeAreaView style={styles.container}>
       <Header />
-      <Body navigation={navigation} />
+      <Body handleNavigateProductDetail={handleNavigateToProductDetail} />
     </SafeAreaView>
   );
 };
@@ -28,56 +32,41 @@ export default HomeScreen;
 
 const Header = () => {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        margin: 20,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+    <View style={styles.header}>
+      <View style={styles.headerLeft}>
         <Image
           source={{
             uri: 'https://cdn-icons-png.flaticon.com/128/5997/5997060.png',
           }}
-          style={{width: 20, height: 20}}
+          style={styles.icon}
         />
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: '700',
-            marginLeft: 10,
-            marginRight: 10,
-          }}>
-          Danh Mục
-        </Text>
+        <Text style={styles.headerTitle}>Danh Mục</Text>
         <Image
           source={{
             uri: 'https://cdn-icons-png.flaticon.com/128/15714/15714554.png',
           }}
-          style={{width: 10, height: 10}}
+          style={styles.iconSmall}
         />
       </View>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <View style={styles.headerRight}>
         <Image
           source={{
             uri: 'https://cdn-icons-png.flaticon.com/128/17820/17820969.png',
           }}
-          style={{width: 20, height: 20, marginRight: 10}}
+          style={[styles.icon, styles.iconMarginRight]}
         />
-
         <Image
           source={{
             uri: 'https://cdn-icons-png.flaticon.com/128/2013/2013141.png',
           }}
-          style={{width: 20, height: 20}}
+          style={styles.icon}
         />
       </View>
     </View>
   );
 };
 
-const Body = ({navigation}) => {
+const Body = ({handleNavigateToProductDetail}) => {
   return (
     <View>
       <FlatList
@@ -85,8 +74,7 @@ const Body = ({navigation}) => {
         horizontal={true}
         data={categories}
         renderItem={({item}) => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate(ScreenEnum.ProductDetail)}>
+          <TouchableOpacity onPress={() => handleNavigateToProductDetail()}>
             <Item item={item} />
           </TouchableOpacity>
         )}
@@ -95,6 +83,17 @@ const Body = ({navigation}) => {
     </View>
   );
 };
+
+const Item = ({item}) => (
+  <View style={styles.itemContainer}>
+    <View style={styles.itemImageContainer}>
+      <Image style={styles.itemImage} source={{uri: item.image}} />
+    </View>
+    <Text numberOfLines={2} style={styles.itemText}>
+      {item.name}
+    </Text>
+  </View>
+);
 
 const categories = [
   {
@@ -123,7 +122,7 @@ const categories = [
   },
   {
     id: '5',
-    name: 'Trà Sữa Trái Cây',
+    name: 'Trà Sữa Trái Cây 2',
     image:
       'https://xingfutangvietnam.com/wp-content/uploads/2021/06/tra-sua-tran-chau-e1685431912430.png',
   },
@@ -141,32 +140,67 @@ const categories = [
   },
 ];
 
-const Item = ({item}) => (
-  <View
-    style={{
-      flexDirection: 'column',
-      width: (width / 10) * 2,
-      alignItems: 'center',
-    }}>
-    <View
-      style={{
-        width: width / 10,
-        height: width / 10,
-        borderRadius: 50,
-        backgroundColor: 'gray',
-        padding: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <Image
-        style={{width: '100%', height: '100%', resizeMode: 'cover'}}
-        source={{uri: item.image}}
-      />
-    </View>
-    <Text
-      numberOfLines={2}
-      style={{fontSize: 16, fontWeight: '500', textAlign: 'center'}}>
-      {item.name}
-    </Text>
-  </View>
-);
+const styles = StyleSheet.create({
+  //HomeScreen
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  //Header
+  header: {
+    flexDirection: 'row',
+    margin: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerTitle: {
+    fontSize: 16,
+    fontWeight: '700',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  headerRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  iconSmall: {
+    width: 10,
+    height: 10,
+  },
+  iconMarginRight: {
+    marginRight: 10,
+  },
+  //Item
+  itemContainer: {
+    flexDirection: 'column',
+    width: (width / 10) * 2,
+    alignItems: 'center',
+  },
+  itemImageContainer: {
+    width: width / 10,
+    height: width / 10,
+    borderRadius: 50,
+    backgroundColor: 'gray',
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  itemText: {
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+});
