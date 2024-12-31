886c714d77c407b833d91ffcb58085dab0de01fb
soda25042004 <147235001+soda25042004@users.noreply.github.com>
2024-12-31 20:15:31 +0700

    debug test no merge

--- src/screens/bottom_navs/VoucherScreen.jsx
+++ src/screens/bottom_navs/VoucherScreen.jsx
@@ -1,18 +1,16 @@
 import { StyleSheet, Image, SafeAreaView } from 'react-native'
 import { View, Text } from 'react-native';
 import NormalHeader from '../../components/headers/NormalHeader';
+import HeaderWithTitleAndBadge from '../../components/headers/HeaderWithTitleAndBadge';
+
+
 
 const VoucherScreen = (props) => {
 
   return (
 
     <SafeAreaView style={styles.container}>
-      <NormalHeader
-        title={"Xác nhận đơn hàng"}
-      />
       <Text>Voucher Screen</Text>
-
-
     </SafeAreaView>
 
 
